308275d323d5991d42b29cbed1d1c15c4c764735
sandpitt-turtle <wottkowicz@gmail.com>
2024-12-03 15:31:49 -0500

    editing on contact page. It is finished as far as the assignemnt goes. I worked to hide the form and present a 'message recieved' response after submitting the form. it seems to have worked out well so far.

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
-//I've started adding a lot of comments for now. I get confused.
-//There are a lot of things I've been using in practice but need 
-//to be able to recreate faster and easier. I got carried away with button stuff. 
-//Its just fun. 
 
 
-'use strict';
+'use strict'; //Idk, I saw this in a tutorial)
 
+
+//Attempting to replace the form with a response message after submission.
+const form = document.getElementById('contact_form');
+const contactMessage = document.getElementById('contact_message')
+const fmessage =document.getElementById('f_message');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  //Adding name of user from form to the message
+
+  const name = document.getElementById('name_text').value;
+
+  fmessage.textContent = `Message Recieved,  ${name}`;
+
+  form.style.display = 'none';
+  contactMessage.style.display = "block";
+});
+
+/*The form should be hidden after submitting 
+and the message should display and add the input under "Name".
+if there is no value entered there, then there simply is nothing 
+written after the default Message*/
+
+
+  
+//FOOTER BUTTON
 const themeToggleButton = document.getElementById('footer-btn');
 const bodyElement = document.body;
+
 
 
 // toggle themes
@@ -39,9 +63,10 @@ function toggleTheme() {
   }
 }
   
-  //Okay, NOW ADDING SCRIPT TO APPLY TO SAVED THEME(S)
-// TO ENSURE THE USER'S CHOICE IS SAVED BETWEEN EACH PAGE
 
+
+  /*OKAY, NOW ADDING SCRIPT TO APPLY TO SAVED THEME(S)
+  TO ENSURE THE USER'S CHOICE IS SAVED BETWEEN EACH PAGE*/
 function applySavedTheme() {
   const savedTheme = localStorage.getItem('theme');
 
@@ -51,20 +76,19 @@ function applySavedTheme() {
     //^ this will check for exisitng theme elements, erase them & then replace them
 
     themeToggleButton.textContent =
-      savedTheme === 'dark-theme' ? 'Switch to Light Theme' : 'Switch to Dark Theme';
+      savedTheme === 'dark-theme' ? 'Switch to Light Theme' : 'Switch to Dark Theme'; 
+      //this is to toggle the button text
   }
 }
+    /*triple = signifies looking for if two particular values are equal in two parameters 
+    (type and value)
+    pretty sure the ? is a faster if/else operator :/
+    when values arent equal to those Dark Theme, light theme will be chosen.
+    .....I think.*/
 
-    //triple = signifies looking for if two particular values are equal in two parameters 
-    //(type and value)
-    //pretty sure the ? is a faster if/else operator :/
-    //when values arent equal to those Dark Theme, light theme will be chosen.
-    //......I think.
-
-
-  
 
 //EVENT LISTENER for button
 themeToggleButton.addEventListener('click', toggleTheme);
 
 applySavedTheme();
+
