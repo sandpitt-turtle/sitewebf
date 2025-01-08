962843be172855bc3dce9578f3019667f7b1446c
sandpitt-turtle <wottkowicz@gmail.com>
2025-01-08 12:02:44 -0500

    init tstest branch

---
+++ src/script.ts
@@ -0,0 +1,79 @@
+"use strict"; // demo said to
+
+//POPUP BAR
+document.addEventListener("DOMContentLoaded", () => {
+  const sidebar = document.getElementById("leftpopbar");
+  const openBtn = document.getElementById("openBtn");
+  const closeBtn = document.getElementById("closeBtn");
+
+  // Add event listeners
+  openBtn.addEventListener("click", () => {
+    console.log("Open button clicked");
+    sidebar.classList.add("leftpopbar-visible");
+  });
+
+  closeBtn.addEventListener("click", () => {
+    console.log("Close button clicked");
+    sidebar.classList.remove("leftpopbar-visible");
+  });
+});
+
+
+//THEME CONTROL
+const themeToggleButton = document.getElementById("footer-btn");
+const bodyElement = document.body;
+
+//toggle
+const toggleTheme = () => {
+  if (bodyElement.classList.contains("dark-theme")) {
+    //to light theme
+    bodyElement.classList.remove("dark-theme");
+    bodyElement.classList.add("light-theme");
+    themeToggleButton.textContent = "Switch to Dark Theme";
+    localStorage.setItem("theme", "light-theme"); // saving to storage
+  } else {
+    //to dark theme
+    bodyElement.classList.remove("light-theme");
+    bodyElement.classList.add("dark-theme");
+    themeToggleButton.textContent = "Switch to Light Theme";
+    localStorage.setItem("theme", "dark-theme"); //saving to storage
+  }
+};
+// Apply saved theme
+const applySavedTheme = () => {
+  const savedTheme = localStorage.getItem("theme"); //fetches 'item'/theme saved in storage
+  if (savedTheme) {
+    bodyElement.classList.remove("light-theme", "dark-theme");
+    bodyElement.classList.add(savedTheme);
+    themeToggleButton.textContent =
+      savedTheme === "dark-theme"
+        ? "Switch to Light Theme"
+        : "Switch to Dark Theme";
+  }
+};
+
+// event listener for theme toggle button
+themeToggleButton.addEventListener("click", toggleTheme);
+applySavedTheme();
+
+// CONTACT FORM
+const form = document.getElementById("contact_form");
+const contactMessage = document.getElementById("contact_message");
+const fmessage = document.getElementById("f_message");
+
+// form
+form.addEventListener("submit", function (event) {
+  event.preventDefault(); // no page refresh
+
+  // const name = document.getElementById("name_text").value; //takes input from form
+
+  const nameInput = document.getElementById("name_text") as HTMLInputElement;
+  const name = nameInput.value;
+
+
+  fmessage.innerHTML = `We have received your message, 
+    <span id="highlighted_name">${name}</span>`;
+
+  form.style.display = "none"; // hides the form
+  contactMessage.style.display = "block"; // Loads success message with transition/animation
+});
